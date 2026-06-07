9fd7a0bad462b1cbfbcdcd1faa27bb6d5976c5f0
Pravesh Shukla <ritupandey199019@gmail.com>
2026-06-08 00:55:27 +0530

    add swagger

---
+++ aviral-api/src/main/java/com/example/aviralbackend/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.example.aviralbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Aviral Vidhya API",
+                version = "1.0",
+                description = "API documentation for Aviral Vidhya ERP System",
+                contact = @Contact(name = "Support", email = "support@aviralvidhya.com")
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
